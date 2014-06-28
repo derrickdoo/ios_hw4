2383bfa52d492d30cf9549eec5862a35deace89f
derricko <derricko@yahoo-inc.com>
2014-06-28 12:33:44 -0700

    Initial Commit

---
+++ ios_hw4/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios_hw4
+//
+//  Created by Derrick Or on 6/28/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
